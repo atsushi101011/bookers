6cbf38185476efaf80ec56bc84df1e7c62f94e4d
EC2 Default User <ec2-user@ip-172-31-31-18.ec2.internal>
2022-02-18 02:02:24 +0000

    CRUD機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,43 @@
 class BooksController < ApplicationController
-  def new
-  end
 
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+  
+  def create
+   @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end  
+  end
+  
+  def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-
-  def show
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+  
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+  end
+  
+  private
+  
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
